92d16aae2a1095873c2616524450f4e7beabc367
TmacAaron <yangyit139@gmail.com>
2025-12-09 17:59:11 +0800

    Update run.sh to comment out LD_PRELOAD export
    Comment out LD_PRELOAD export due to platform availability.

--- examples/parallelism/run.sh
+++ examples/parallelism/run.sh
@@ -2,7 +2,8 @@ export HCCL_OP_EXPANSION_MODE="AIV"
 export TASK_QUEUE_ENABLE=2
 export CPU_AFFINITY_CONF=2
 
-export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so.2:$LD_PRELOAD
+# libjemalloc is not available on all platform
+# export LD_PRELOAD=/usr/lib/aarch64-linux-gnu/libjemalloc.so.2:$LD_PRELOAD
 
 
 FLUX_DIR=/home/weights/FLUX.1-dev/ torchrun --nproc_per_node=1 run_flux_cp_npu.py --attn "_native_npu" --height 1024 --width 1024
